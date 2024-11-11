aa7aec38bef237637f2cc694f4cf0741a1db000a
Jagadeesh987-mouli <mouli7332@gmail.com>
2024-11-11 14:50:58 +0530

    error rectified

--- P01_GIT-Demo/src/main/java/com/jm/config/AppConfig.java
+++ P01_GIT-Demo/src/main/java/com/jm/config/AppConfig.java
@@ -2,9 +2,11 @@ package com.jm.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan(basePackages="com.jm")
+@PropertySource("com/jm/files/myFile.properties")
 public class AppConfig 
 {
  	
