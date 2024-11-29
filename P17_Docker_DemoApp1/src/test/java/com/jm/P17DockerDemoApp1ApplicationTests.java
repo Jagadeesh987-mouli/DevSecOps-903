94b39ca5e6080754b55df74fb944dc8e113fb013
Jagadeesh987-mouli <mouli7332@gmail.com>
2024-11-29 13:44:06 +0530

    commit App1

---
+++ P17_Docker_DemoApp1/src/test/java/com/jm/P17DockerDemoApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P17DockerDemoApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
